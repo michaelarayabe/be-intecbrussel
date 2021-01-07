2de23a7a0719aaf12c1e21f5df5ff106f0b48a95
michaelarayabe <michaelseifuaraya@gmail.com>
2021-01-07 15:27:38 +0100

    first commit

---
+++ src/main/java/guru/springframework/beintecbrussel/BeIntecbrusselApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.beintecbrussel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeIntecbrusselApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeIntecbrusselApplication.class, args);
+    }
+
+}
